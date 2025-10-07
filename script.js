51f8324dd0be349fe591bbc79704a3b4bb1c872b
Ilhan Diler <ilhan.diler@gmail.com>
2025-10-07 08:19:39 +0200

    Bildüeberschrift Musik baut Brücken hinzugefügt als .jpg

--- script.js
+++ script.js
@@ -154,6 +154,7 @@ window.addEventListener('DOMContentLoaded', function() {
 // Hero-BG Gallery/Slideshow
 const heroGallery = [
      { src: 'bilder/Gruppenbild2.jpg', title: 'Benefizkonzert für „Ärzte ohne Grenzen“ 2025 im Maintz Hotel Ochsenfurt' },
+      { src: 'bilder/gruppe_2007.jpeg', title: 'CMI 2007' },
      { src: 'bilder/peterbild.jpg', title: 'Gedenkfeier - „80 Jahre Kriegsende, 80 Jahre Frieden in Ochsenfurt 2025' },
      { src: 'bilder/concello.jfif', title: 'Concello 2025 in der Klosterkirche Ochsenfurt' },
      { src: 'bilder/gruppemitflagge.jpg', title: 'Gruppenbild 2025' },
@@ -164,7 +165,7 @@ const heroGallery = [
      { src: 'bilder/Scheunenkonzert 17.07.jpg', title: 'Scheunenkonzert 17.07.2017' },
 
      { src: 'bilder/Gruppe17.09.11.jpg', title: 'Jubiläumskonzert zum 30-jährigen Bestehen am 17.09.11' },
-       
+    
      //  { src: 'bilder/konzert.jpg', title: 'Benefizkonzert für „Ärzte ohne Grenzen“ 2025 im Maintz Hotel Ochsenfurt' },
   //   { src: 'bilder/maria_schnee_neujahrskonzert_2023.png', title: 'Neujahrskonzert 2023' },
    //  { src: 'bilder/Gruppenbild.jpg', title: 'Collegium Musicum Iuvenale 2025' },
@@ -239,6 +240,7 @@ function setHeroBgCrossfade(idx) {
         }
     } else {
     if (
+        heroGallery[idx].src.includes('bilder/gruppe_2007.jpeg') ||
         heroGallery[idx].src.includes('bilder/Ochsenfurt 23.07.2005.JPG') ||
         heroGallery[idx].src.includes('bilder/Opern-Gala-BGS - 20 von 44.jpg') ||
         heroGallery[idx].src.includes('bilder/Ochsenfurt 05.03.2011.JPG') ||
